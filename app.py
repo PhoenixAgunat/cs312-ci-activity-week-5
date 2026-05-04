748a20b66e8108a629f6b8eb288a4e4085d83323
basic_stats.py <agunatp@oregonstate.edu>
2026-05-03 20:29:01 -0700

    Fix flake8 formatting

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-# Name: Phoenix Agunat
-# Date: 5/3/2026
-# Activity: Week 5 activity 2
-
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return jsonify({"status": "ok", "version": "1.0.0"})
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+    
